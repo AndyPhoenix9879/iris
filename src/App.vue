f6d0002de6cf635cf7f83e0e0581eafbb1037f0f
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-05 19:52:15 +0800

    Added navigation bar
    Also some other stuff like some edit in About page and other other stuffs

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,58 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+      <div class="navEmblem">
+        <b> Lai's Webpage </b>
+      </div>
+
+      <div class="navItem">
+        <router-link to="/home">Home</router-link> <el-divider direction="vertical"></el-divider>
+        <router-link to="/about">About</router-link> <el-divider direction="vertical"></el-divider>
+        <router-link to="/asf">Resources</router-link> <el-divider direction="vertical"></el-divider>
+        <router-link to="/asfasf">Test</router-link> <el-divider direction="vertical"></el-divider>
+        <router-link to="/af">Test2</router-link> 
+      </div>
+
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding: 20px;
+  background-color: white;
+  box-shadow: 0px 6px 6px -2px rgba(0,0,0,0.10);
+  position: sticky;
+  position:-webkit-sticky;
+  display: flex;
   a {
+    text-decoration: none;
     font-weight: bold;
-    color: #2c3e50;
+    color: #303133;
+    transition: linear 0.2s;
     &.router-link-exact-active {
-      color: #42b983;
+      color: #67C23A;
+      transition: linear 0.2s;
     }
+  }
+}
+.navEmblem {
+  width: 100%;
+  margin-left: 50px;
+}
+.navItem {
+  text-align: right;
+  width: 100%;
+  a {
+    padding: 0 15px 0 15px;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    //add whatever component name you'll have here
   }
 }
 </script>
+
+<style scoped>
+
+</style>
+
