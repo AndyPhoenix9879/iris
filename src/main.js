358802dc8e436b40170c14aca247cba52c34857d
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-05 19:06:23 +0800

    Added element UI and bootstrap component library for Vue

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import 'popper.js';
+import 'bootstrap'; //only for JS file
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(ElementUI, { locale });
 
 new Vue({
   router,
