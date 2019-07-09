aa4371b3c6ac43a6b074932453f2b702e97e1f01
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-09 15:50:39 +0800

    Profile.vue: Initial creation

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mainContainer">
+        <img src="https://lh3.googleusercontent.com/Mxtu2XLm6YKuDHMwJOiDkbi3mPF1HbhpNvZTGVt-GH1L3AGSmsuzFinn1n_ymn3-PKqcH12Zl-l5iA2UCgkQlViJMAmDvDVN_tiW3EUFitbZxf0ggidcK3G-yTbzapmvhwgnzTuccDmw24geEtfOlqMnu4ttzkRkS93EueOi7aLBUVpKMMBd0WVisl8e1c8cskEwTCNwKBIEJ7aVfi6SGNZbPQCol42xSbsbkLfgWLiip_DVboXhoFEplrbbeW2fQxvU5MRaodF1s7ZpwaE7FiLp1Pruvq24eTwYbDKtUGx9Z8JlrITF6wy68lXIC5a-igD5G9xUzNcmp-d3XYZNxNk8nuHjB2Pg48r5vCIonV9BhKe84esww8A1FJjy8IDT2-w78Lj1kMfCB1F6grjxhynvz3_hEpacDR99rLCqsdIAEag2k58y4XwyGllLUJW45FZoikoXziyh4n1KtDdgcrO58vohwpzso3HYYgQIwo2KE4hc2bOSN1ZvaFt9Gus5D1CRz6qL-EZq11K0p3FUmhRezi712-DaW9GEfw4TvopYSBMHlXAgqlcL-gUSZghEyKkZKcqQli3zCNhFRQNVut3V3e0zN_PYo4g0x4wTdCAJeRyAKp9njrWIykTYjeimzyjS1tBdo_6zBxbrcQI5F-3C_1DJKG--=w3342-h1878-no"
+        style="width:100%; height:100%; position:fixed; z-index:-9999; top:0;">
+
+        <div class="Card">
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
