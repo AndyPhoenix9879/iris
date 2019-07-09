8172915edfa78fc0e98c021183d10d474e5aebf2
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-09 14:58:53 +0800

    Contact.vue: Added some content

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,16 @@
     <div class="mainContainer">
         <img src="https://lh3.googleusercontent.com/KTxN-f7PA6PKyBnSE3Pmr20kd7kpD-qkan-xqUN_ZKoJ398aXFAm-K-1WZ0l3iHwjVoNuhpsRSs80M0TH62zHm3KeFheYVsbBhOFKWhXPtRE-2xOPt15v5_3hEatV5qwVQBuaG3fdPVoTVjk2KaQVpbKj4gItVGp45Z8kF5IX2A2ndlyMrEjokZw0-YKWUEddwSeSyQ7ehS1BkQSy1c2WWn5SsUu_D6_67dBIYLVl_GEJIVjtxBDvTWVEbMlyDleBkSzo56XT7UNWaD2b2DDPwqt4sa8jTeNAkOuEjXpQK4F47oX5lSEAL3jkN1q2D56q3Zbv1pULkikZV2J4919zleL4_W6-mRREAyE_HKRPEA80Zt6S6STWlHSXxyOt26NDqIEpuybaGGGs-FqkxTolQqrMnAZm0CeTNo3ZSq8IuLeKZugrlJlFs5LV5tcX5aKr2HRl0AqTxHevGcD7aRGPp1MncdPdKhhXU5jDjmVCBxCuj9WFJwwNX_4b8lK_A9eYyASO8UWWpr6d3F8Iov2agZjdLbbWB8Nvx4IF_zwj1UaA4G0sYB9Xe1349YvsGxcISJWFRJaPAK60WNj8aJ-Wu5z7wcqXlPDCHG1pexJHs5C3ceZgoikThf_ziCAj52oc0zqZm8Ko_fYJuWYGi1jGOCfY0sfYt97=w3342-h1878-no"
         style="width:100%; height:100%; position:absolute; z-index:-9999; top:0;">
+
+        <div class="Card">
+            <h1>Contact details: </h1>
+            <hr>
+            <p>Tel: +852 69949398 <br>
+            Email: Lai9879m@gmail.com</p>
+            <hr>
+            <button></button>
+            <hr>
+        </div>
     </div>
 </template>
 
