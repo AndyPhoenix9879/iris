2ebb6c3ddd375e5f96019a3d3ef81ca8ab9b0068
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-09 15:29:41 +0800

    background: changed CSS position to fixed instead of absolute
    Reason being the fact that absolute isn't dynamic enough i.e. it's sorta just pasted onto the wall and you can scroll pass it. Fixed would allow scrolling of the background as well so it kinda move along when you scroll

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mainContainer">
     <img src="https://lh3.googleusercontent.com/MHK2iH8xPP9mmlA4EPxdEYj-6N8VZ6zEsB9mqQ6ab33uM7T6ZMHXFk9ZiLx-DE3BV5n6XXF3cnn5MAIBgWES9VJxTgqwMeN3S6rYa_zhPUMpPScV7CvfxCdTRaFBVSQ7iShp7uEd86buWi8cOeX9yBNdY2wBLtiUj2cEpZb2XjoDFY9ES_MliAzDGPMqkoHti1jUi31bjrorE1V4sk_oBp9svUrW-1HNXRzq5ZcbnxzfxeMrE9Y9-Tg2g7E63_1XKPm7BLmTa5AUYkcLjjs50RgcEZ7eAN9qjrRgaquAT52tEhfJ505ZVFCkVgx0eSKdt2bf7aa3xpmG59o8s3PMilOQJPQbIj1gJGreDkvQw7TYT2U8dOPKVR8JVFIZX0LTplKbJtEYbZVh7C3VLGxgHhY95VdkcdZRUhE-sJSSGgoIWrZyQLktlY0O1llzI1Mex9j8jvy_Pavpci4qmh1l9Chb0_tIDV_lLDTNG85ns3obXitvl4aAvdMHSHBLbT-4e1H4qp8yvPKuN3HkE4v857MRIeVZxG-qlQQbYBJTagfWL4Eii0HSwzGZ8USMESRGjlnhyt3A2wOwOcsflxAaP1o1lUVyodHR1otvG-ZRIRVtGKbaZHQBUVJ-cow_-47Fq5Z9YxpY92ZPKeeCgtwMJ3NJEjd7dFZ-=w3342-h1878-no"
-    style="width:100%; height:100%; position:absolute; z-index:-9999; top:0;">
+    style="width:100%; height:100%; position:fixed; z-index:-9999; top:0;">
     <div class="Card">
       <h1>About this website</h1> 
       <p>
@@ -25,11 +25,5 @@ export default {
 </script>
 
 <style scoped>
-.Card {
-  border-radius: 20px;
-  padding: 30px 100px 30px 100px;
-  width: 100%;
-  background-color: white;
-  box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.06);
-}
+
 </style>
