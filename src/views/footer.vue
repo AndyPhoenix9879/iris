ad23ad33640be1c07e3f010cc94589b1d3e3a053
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-15 16:49:59 +0800

    footer.vue: remove an el-divider

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -4,7 +4,7 @@
             <a target="_blank" href="https://www.linkedin.com/in/laijianshin9879/"><img src="../assets/Circle Color/Linkedin.png"></a> <el-divider direction="vertical"></el-divider>
             <a target="_blank" href="https://github.com/AndyPhoenix9879"><img src="../assets/Circle Color/Github.png"></a> <el-divider direction="vertical"></el-divider>
             <a target="_blank" href="https://www.facebook.com/andy.lai.9879"><img src="../assets/Circle Color/Facebook.png"></a> <el-divider direction="vertical"></el-divider>
-            <a target="_blank" href="https://www.instagram.com/lai9879/"><img src="../assets/Circle Color/Instagram.png"></a> <el-divider direction="vertical"></el-divider>
+            <a target="_blank" href="https://www.instagram.com/lai9879/"><img src="../assets/Circle Color/Instagram.png"></a> 
         
         <br><br><br>
         Developed by Lai Jian Shin using vueJS.<br>
