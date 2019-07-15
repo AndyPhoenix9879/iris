9cea1a928bebaeeb198591c5dcc459c0fddcb65b
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-15 15:47:38 +0800

    Profile.vue: footer content rearrangement

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -90,13 +90,16 @@
         </div>
     </div>
     <div class="footer">
-        Find me on social media: 
-            <el-divider direction="vertical"></el-divider>
+        
             <a target="_blank" href="https://www.linkedin.com/in/laijianshin9879/"><img src="../assets/Circle Color/Linkedin.png"></a> <el-divider direction="vertical"></el-divider>
             <a target="_blank" href="https://github.com/AndyPhoenix9879"><img src="../assets/Circle Color/Github.png"></a> <el-divider direction="vertical"></el-divider>
             <a target="_blank" href="https://www.facebook.com/andy.lai.9879"><img src="../assets/Circle Color/Facebook.png"></a> <el-divider direction="vertical"></el-divider>
             <a target="_blank" href="https://www.instagram.com/lai9879/"><img src="../assets/Circle Color/Instagram.png"></a> <el-divider direction="vertical"></el-divider>
         
+        <br><br><br>
+        Developed by Lai Jian Shin using vueJS.<br>
+        Copyright 2019 Lai Jian Shin. All rights reserved.
+
     </div>
 </div>
 </template>
