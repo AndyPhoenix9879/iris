4479ed0aa6d7a389a5eb1e37ce8d559ecb655d33
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-16 14:08:52 +0800

    About.vue: content changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,11 +7,8 @@
       <h1>Hey there!</h1><br>
       <img src="https://lh3.googleusercontent.com/KC2YBg-D6PDUkgx3qQqrlxWYYthSkpgOJgpqHIky_jj_8Y8Up-gMFMlluaCYoKemZ37VmUiNH6ZOGf0LBwtsaoTDrqYr820SjZMV1wb1KnIG8eGe5ZJYZkBbKJTnTR47N5R3XX36DxglZaOoyUoVHdad5EngvyqCzrAZN_QuzALqW3gYgtj8WJh8wnaJtH-08fFX9SJZjJg1UuBU2EAp5dxWOqyuJRlsxj40V64xJKkX_wpqMZB20mAfnmYgqe820Hrmfp3aFr3qaUm-m91QG_SZo-HlgKErTPONu2lQQzdU0B7qPvNoItBqmlm7rMf-fQdiwJYfYR_BleQac_BNAG6b-xwZn3a1N6ilE_s_xFJjzBGgNa3FjxgoVluizMtixvfkLcOZ0-LZJJFCqN5eiqAQC1TNwiCLTCuxas2WmSNd69YjZF8iP7RYs5l1W9qG6JRStXj2pRMfRPPK_jhIGvKQI--t-Cg4JKyl2svM5boGFyd2IxTav6bNdcT6_AZg6polBPHauGIO_5yIL3gLtyXCS8rRWUEfZxkBIeW5G7EHs_xErEDQxzC58sY_JtxP5qcIeVaPbW9qcsiUP3b5_0CL9kS5apgpZs6hV9YocWF8sFQLMiEwtV0aJ2EVHTdcvRW8GLPsrJFmDRi8Mo2eU4vfX9e-Bd7s3flu84WWZ7VWaMTxzbE6AVnT1tNFCPo1fQ5q7FcSkFEMZ60B1ik7HpEZ-w=w1964-h1878-no"
       alt="image" style="border-radius:50%; width:50%;"><el-divider></el-divider>
-      <p>Welcome to my webpage. Some brief introduction of myself: <br><br> My name is Lai Jian Shin, I'm a student in the Chinese University of Hong Kong
-        pursuing a degree in Computer Science and expected to graduate in the year 2022. I'm an international student, from Brunei Darussalam and I have a passion 
-        for technology (mostly on mobile and processor), photography and software development. My experience
-        in programming language includes C, VB.NET, Python and Javascript. <br><br>
-        Feel free to contact me if you wish, and thanks for visiting my webpage! </p> <el-divider></el-divider>
+      <p>Welcome to my webpage! <br> Some information about myself, my name is Lai Jian Shin, I'm an international undergraduate student at the Chinese University of Hong Kong pursuing
+        a degree in Computer Science.</p>
     </div>
     
     <div class="Card">
@@ -19,8 +16,9 @@
       <p>
         
         <el-divider></el-divider>
-        This website is the culmination of everything I have learnt in Vue.JS, web development and UI design (as well as abstract digital drawing). This website can be regarded as my playground project
-        where I play with whatever there is to play around with in Web Development. Other than that, I will use this as my platform to share stuffs I think is either helpful
+        This website is the culmination of everything I have learnt in Vue.JS, web development and UI design (as well as some abstract digital drawing). This website can be 
+        regarded as my playground project where I play with whatever there is to play around with in Web Development. 
+        Other than that, I will use this as my platform to share stuffs I think is either helpful
         or cool around the web as well as a CV profile tab just for the sake of it. Enjoy! 
         <el-divider></el-divider>
         
