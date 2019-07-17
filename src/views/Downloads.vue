ea4a99c974e402217be4f0e2e75a678d7879338d
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-17 11:10:35 +0800

    Downloads.vue: What if the thing we want to search doesn't exit?

--- src/views/Downloads.vue
+++ src/views/Downloads.vue
@@ -11,10 +11,12 @@
         <img src="https://lh3.googleusercontent.com/PPLEBPbQjUrMRleHOiDUnuvfLK_6XRKeUbMohSMGpk-NYaZnxvSUTM8awSJFmqvzt9gAZwt0xSE1XkqEzYrH9ZG3gyWMIbYpPCXP_Xx_BxMoPGWPK_CmTSSvYPPeynB99jf8W9RT2_yXkKRN55T_F_9BAGyadi42q4zS5fRmLUJyU9g_pRLGXaqr32B3qETItxLynw7LxafPB2sV6w4fG4AA61R2pGFALVuTyk4cjTTEDMFQw4HM-edtnGW7FzDmeaUU0MOzAMSnJbPDR_-JJfALKcLbzDw_59yCIYNpsQIeMAqXbuVMv8nmjd7oITono7_3_ijOZa9R3D_W2UOE2jviopBncNvv0xBlThng2Ki9yfAfHOc2FcsKSiB0HhMJA1QnFdznxyB3Wk8wXO52OkY5ABpofmP4Ka1AfDcGXfUQjkirkUDWu0QsFfUCwN3o5IvBPdQ4AD0Q6N0MmPyoZpsdVcoBjpRu7rRnPyo1WG0ldhdsHXGe6bzClggVpj_SSJpT1PdAUhRCBONmPUSiZKPl2HWF6Fuq_hbJXMyV9-jenxJ7CeH-7IcTAMxeTJxEYvaOB9OLBQUfMcRt6CdRAGUKqXFpepro6U0o_DyvaeeyHI_c--yROCaOAnjG2fUE_iaVj8StPPIvDQxM7xNJApFWuaOoxCYh=w3166-h1780-no"
         style="width:100%; height:100%; position:fixed; z-index:-9999; top:0; min-width: 1000px;">
         <div class="tray">
+            <div class="notFound" v-if="filteredSearch==''">
+                <h1>No results... :(</h1>
+            </div>
             <div class="Card" v-for="(value, index) in filteredSearch" v-bind:key="value.index">
-                <!--<el-button type="danger" circle icon="el-icon-minus" id="min" @click="remove(index)"></el-button>-->
+                <!--<el-button type="danger" circle icon="el-icon-minus" id="min" @click="remove(index)"></el-button>--> 
                 <div class="CardContent">
-
                     <a :href=value.url style="outline:none;" target="_blank">
                         <div class="TypeBox" v-if="value.type==='pic'">
                             <img :src="value.url">
@@ -29,9 +31,10 @@
                     </a>
 
                 </div>
+               
                 
             </div>
-
+            
             <!--<el-button type="success" circle icon="el-icon-plus" id="add" @click="addList"></el-button>-->
         </div>
     </div>
@@ -124,6 +127,11 @@ export default {
     margin: auto;
     padding: 13px;
     padding-left: 20px;
+}
+.notFound {
+    color: #909399;
+    padding-top: 40px;
+    padding-bottom: 25px;
 }
 #add {
     box-shadow: 0px 3px 10px 0px rgba(0,0,0,0.15);
