107f10f11edd9c37711ecb4d91f73c99d5aeb869
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-18 09:53:25 +0800

    Downloads.vue: footer changes

--- src/views/Downloads.vue
+++ src/views/Downloads.vue
@@ -1,15 +1,12 @@
 <template>
 <div>
-    
-    <div class="mainContainer" style="position:relative">
+
+    <div class="mainContainer" style="position:relative;">
 
         <div class="searchContainer">
             <i class="el-icon-search" style="position:absolute;"></i>
             <input v-model="search" placeholder="Search stuffs" class="traySearch">
         </div>
-
-        <img src="https://lh3.googleusercontent.com/PPLEBPbQjUrMRleHOiDUnuvfLK_6XRKeUbMohSMGpk-NYaZnxvSUTM8awSJFmqvzt9gAZwt0xSE1XkqEzYrH9ZG3gyWMIbYpPCXP_Xx_BxMoPGWPK_CmTSSvYPPeynB99jf8W9RT2_yXkKRN55T_F_9BAGyadi42q4zS5fRmLUJyU9g_pRLGXaqr32B3qETItxLynw7LxafPB2sV6w4fG4AA61R2pGFALVuTyk4cjTTEDMFQw4HM-edtnGW7FzDmeaUU0MOzAMSnJbPDR_-JJfALKcLbzDw_59yCIYNpsQIeMAqXbuVMv8nmjd7oITono7_3_ijOZa9R3D_W2UOE2jviopBncNvv0xBlThng2Ki9yfAfHOc2FcsKSiB0HhMJA1QnFdznxyB3Wk8wXO52OkY5ABpofmP4Ka1AfDcGXfUQjkirkUDWu0QsFfUCwN3o5IvBPdQ4AD0Q6N0MmPyoZpsdVcoBjpRu7rRnPyo1WG0ldhdsHXGe6bzClggVpj_SSJpT1PdAUhRCBONmPUSiZKPl2HWF6Fuq_hbJXMyV9-jenxJ7CeH-7IcTAMxeTJxEYvaOB9OLBQUfMcRt6CdRAGUKqXFpepro6U0o_DyvaeeyHI_c--yROCaOAnjG2fUE_iaVj8StPPIvDQxM7xNJApFWuaOoxCYh=w3166-h1780-no"
-        style="width:100%; height:100%; position:fixed; z-index:-9999; top:0; min-width: 1000px;">
 
         <div class="tray">
 
@@ -121,6 +118,9 @@ export default {
 }
 </script>
 <style scoped>
+.mainContainer {
+    text-align: center;
+}
 .searchContainer {
     height: auto;
     width: 100%;
@@ -249,9 +249,6 @@ export default {
     bottom: 0;
     position: absolute;
 }
-.footer {
-    position: fixed;
-    bottom: 0;
-}
+
 </style>
 
