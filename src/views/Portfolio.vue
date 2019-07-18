5ccaf8673e1cf8cd54ef3e13f3274ec28d863d9a
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-18 10:46:11 +0800

    Portfolio.vue: Initial creation

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="mainContainer">
+        </div>
+        <webfooter/>
+    </div>
+</template>
+<script>
+import webfooter from './footer'
+export default {
+    components: {
+        webfooter
+    }
+}
+</script>
+<style scoped>
+
+</style>
