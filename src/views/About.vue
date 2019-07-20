4c7515738b50ccf7bab2a6f33c92be637bd70e94
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-07-20 12:13:15 +0800

    About.vue: Added content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,16 @@
 <template>
 <div>
   <div class="mainContainer">
-    
-    <h1><br><br><br> THE STUFF</h1>
+
+    <div class="aboutTitle"><b>A Fun Experiment...</b></div>
+    <span style="font-size:30px">
+      that's all there is to this website. And more of course. But I developed this website
+      as a "playground" for me to explore and learn web development using a framework (Vue.JS).
+      <br><br>
+      Beyond that, the content here is for me to share resources in a centralized and easy to access
+      manner - and these resources ranges anywhere from academic to stuffs I think would be helpful or
+      interesting or just really cool. 
+    </span>
     
   </div>
 
@@ -22,6 +30,10 @@ export default {
 
 <style scoped>
 .mainContainer {
-  min-height:620px;
+  margin: 150px 0 180px 0;
+  min-height:470px;
+}
+.aboutTitle {
+  font-size: 100px;
 }
 </style>
