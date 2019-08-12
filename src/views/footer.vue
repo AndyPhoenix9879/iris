d9add1f08b0706a91837650545161e113b4abf76
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-08-12 14:20:32 +0800

    footer.vue: optimized the 4 option thingy in the footer

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="footer">
 
-        <div class="fir" @click="unshowAll">
+        <div class="fir">
         </div>
 
         <div class="sec">
-            <div class="fIcon" @click="unshowAll">
+            <div class="fIcon">
                 <a target="_blank" href="https://www.linkedin.com/in/laijianshin9879/"><img src="../assets/Circle Color/Linkedin.png"></a> 
                 <a target="_blank" href="https://github.com/AndyPhoenix9879"><img src="../assets/Circle Color/Github.png"></a> 
                 <a target="_blank" href="https://www.facebook.com/andy.lai.9879"><img src="../assets/Circle Color/Facebook.png"></a> 
@@ -21,7 +21,7 @@
                         <p>In greek mythology, Iris was the goddess of rainbow and the messenger of the Gods. <br><br>This webpage aims
                         to impersonify those attributes. The design I implemented hopes to give off a colourful and friendly look, and the
                         content I hope would be a good repository of resources that we all could search and find easier</p>
-                        <button @click="showAbt" id="abt" slot="reference">About</button>
+                        <button id="abt" slot="reference">About</button>
                     </el-popover>
                 </div>
 
@@ -32,7 +32,7 @@
                         trigger="click">
                         <p>Mobile Tel : +852 69949398<br>
                         Email : <a href="Lai9879m@gmail.com">Lai9879m@gmail.com</a></p>
-                        <button @click="showContact" id="cntct" slot="reference">Contact</button>
+                        <button id="cntct" slot="reference">Contact</button>
                     </el-popover>
                 </div>
 
@@ -46,13 +46,20 @@
                         placement="top"
                         title="Legend"
                         trigger="click">
-                        <p>WEB</p>
-                        <button @click="showLegend" id="legend" slot="reference">Legend</button>
+                        <p>
+                            WEB = Webpage<br>
+                            GDRIVE = Google Drive<br>
+                            PDF = PDF<br>
+                            TOR = Torrent file<br>
+                            DOCX = MS Word file<br>
+                            PIC = Picture, usually JPG
+                        </p>
+                        <button id="legend" slot="reference">Legend</button>
                     </el-popover>
                 </div>
 
             </div>
-            <div class="theBottomText" @click="unshowAll">
+            <div class="theBottomText">
                 Developed by Lai Jian Shin.<br>
                 <span style="color: #67C23A; font-weight: bolder;">vueJS</span> + 
                 <span style="color: #409EFF; font-weight: bolder;">ElementUI</span> + 
@@ -60,7 +67,7 @@
             </div>
         </div>
 
-        <div class="thir" @click="unshowAll">
+        <div class="thir">
         </div>
         <!-- <img src="../../public/iris.png" style="height: 30px; margin-top: -2.5px; margin-left: -7px;"> was developed as a centralized web page for me to share my resources in a prettier way :) <br>
         -->
@@ -74,37 +81,14 @@ export default {
         }
     },
     methods: {
-        showAbt() {
-            var target = $("#abt");
-            target.hasClass("shown")? target.removeClass("shown") : target.addClass("shown");
-            var otarget = $("#legend , #cntct")
-            otarget.hasClass("shown")? otarget.removeClass("shown") : otarget.addClass("");
-        },
-        showContact() {
-            var target = $("#cntct");
-            target.hasClass("shown")? target.removeClass("shown") : target.addClass("shown");
-            var otarget = $("#legend , #abt")
-            otarget.hasClass("shown")? otarget.removeClass("shown") : otarget.addClass("");
-        },
-        showLegend() {
-            var target = $("#legend");
-            target.hasClass("shown")? target.removeClass("shown") : target.addClass("shown");
-            var otarget = $("#abt , #cntct")
-            otarget.hasClass("shown")? otarget.removeClass("shown") : otarget.addClass("");
-        },
-        unshowAll() {
-            var otarget = $("#legend, #cntct, #abt");
-            
-            otarget.hasClass("shown")? otarget.removeClass("shown") : otarget.addClass("");
-            
-        }
+        
     }
 }
 </script>
 <style scoped>
 .footer {
   text-align: center;
-  height: 230px;
+  height: 250px;
   background-color: white;
   padding: 30px 0 30px 0;
   box-shadow: 0px -6px 6px -2px rgba(0,0,0,0.10);
@@ -144,8 +128,9 @@ export default {
 .fgrid div a:hover {
     color: #409EFF;
 }
-.fgrid div a:active {
+.fgrid div a:focus {
     color: #F56C6C;
+    font-weight: bolder;
 }
 .fgrid div button {
     border: 0;
@@ -157,23 +142,12 @@ export default {
     color: #F56C6C;
     cursor: pointer;
 }
-.fgrid div button:active {
+.fgrid div button:focus {
     color: #F56C6C;
+    font-weight: bolder;
 }
 .theBottomText {
     padding-top: 60px;
-}
-#abt.shown {
-    color: #F56C6C;
-    font-weight: bolder;
-}
-#cntct.shown {
-    color: #F56C6C;
-    font-weight: bolder;
-}
-#legend.shown {
-    color: #F56C6C;
-    font-weight: bolder;
 }
 @media screen and (max-width: 970px) {
   .footer {
